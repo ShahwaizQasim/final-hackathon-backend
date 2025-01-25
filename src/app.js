1baaa50436c2faa4bf4ab1bdbbb9578e764e5932
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-25 19:04:37 +0500

    route delete

--- src/app.js
+++ src/app.js
@@ -11,10 +11,6 @@ app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.get('*', (req, res) => {
-    res.status(404).json({ message: 'Route not found!' });
-});
-
 app.use('/api', router)
 
 export { app }
