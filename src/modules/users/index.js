7b4c31da6b0ad4b5f804ef9b88b6c563874b5f27
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-25 16:54:49 +0500

    user verify

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -6,12 +6,13 @@ import DeleteUser from "./controllers/deleteUser.js";
 import LoginUser from "./controllers/loginUser.js";
 import UserAuthenticate from "../../middlewares/AuthenticationUsers.js";
 import FindUser from "./controllers/findUser.js";
+import VerifyUserAuthentication from "../../middlewares/VerifyUserAuthenticate.js";
 
 const userRouter = Router();
 
 userRouter.get('/', UserAuthenticate, GetUser);
 userRouter.post('/login', LoginUser)
-userRouter.post('/UserInfo', FindUser)
+userRouter.post('/UserInfo', VerifyUserAuthentication, FindUser)
 userRouter.post('/', AddUser)
 userRouter.put('/:id', UpdateUser)
 userRouter.delete('/:id', DeleteUser)
